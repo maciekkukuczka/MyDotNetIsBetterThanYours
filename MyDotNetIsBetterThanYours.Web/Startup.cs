59499ae93dd8551bf03791e374e3519f081c8351
Maciek Kukuczka <maciek@kukuczka.net>
2020-08-17 08:55:23 +0200

    Deploy configuration

--- MyDotNetIsBetterThanYours.Web/Startup.cs
+++ MyDotNetIsBetterThanYours.Web/Startup.cs
@@ -64,19 +64,22 @@ namespace MyDotNetIsBetterThanYours.Web
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
+
         {
+            app.UseDeveloperExceptionPage();
+            app.UseDatabaseErrorPage();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
+
+            app.UseExceptionHandler("/Error");
+
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
